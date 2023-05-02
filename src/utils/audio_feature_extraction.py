d7af9b0e14ddf6817867a7e2bd05f722706b53f5
kanielv <kanielvicencio@gmail.com>
2023-05-02 18:58:23 -0400

    added audio utils, updated requirements.txt

---
+++ src/utils/audio_feature_extraction.py
@@ -0,0 +1,54 @@
+'''
+This file contains functions for audio feature extractions
+Author: Kaniel Vicencio
+'''
+
+import librosa
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Waveform
+def get_waveform(fpath): 
+    signal, sample_rate = librosa.load(fpath)
+    return signal, sample_rate
+
+# feature extraction: mel-freq cepstral coefficients 
+def get_mfccs(y, sr):
+    df_mfccs = pd.DataFrame()
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=12)
+    norm = mfccs / np.linalg.norm(mfccs)
+
+    for n_mfcc in range(len(norm)):
+        df_mfccs['MFCC_%d'%(n_mfcc+1)] = norm.T[n_mfcc]
+
+    return df_mfccs, norm
+
+# feature extraction: mel spectogram
+
+def get_melspectrogram(y, sr):
+    df_melspectrogram = pd.DataFrame()
+    mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=12)
+    norm = mel_spectrogram / np.linalg.norm(mel_spectrogram)
+
+    for n_mel in range(len(norm)):
+        df_melspectrogram['Mel_Spectogram_%d'%(n_mel+1)] = norm.T[n_mel]
+
+    return df_melspectrogram, norm
+
+# feature extraction: chroma
+def get_chromagram(y, sr):
+    df_chroma = pd.DataFrame()
+    chromagram = librosa.feature.chroma_stft(y=y, sr=sr)
+    norm = chromagram / np.linalg.norm(chromagram)
+
+    for n_chroma in range(len(norm)):
+        df_chroma['Chroma_%d'%(n_chroma+1)] = norm.T[n_chroma]
+
+    return df_chroma, norm
+
+# generate a feature matix with the 3 features 
+def generate_feature_matrix(df_mfccs, df_chroma, df_melspectrogram):
+    feature_matrix = pd.concat([df_mfccs, df_chroma, df_melspectrogram], axis=1)
+    return feature_matrix
+
---
+++ src/utils/data_visualization.py
@@ -0,0 +1,52 @@
+'''
+This file contains a bunch of utility functions for data visualization
+Author: Kaniel Vicencio
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import librosa 
+import librosa.display
+def plot_waveform(y, sr, name):
+
+    librosa.display.waveshow(y, sr=sr)
+    plt.title(name)
+    plt.xlabel('Time / second')
+    plt.ylabel('Amplitude')
+
+    plt.show()
+
+def plot_frquency(y, sr, name):
+    
+    k = np.arange(len(y))
+    T = len(y)/sr
+    freq = k/T
+
+    DATA_0 = np.fft.fft(y)
+    abs_DATA_0 = abs(DATA_0)
+
+    plt.title(name)
+    plt.plot(freq, abs_DATA_0)
+    plt.xlabel('Freq / Hz')
+    plt.ylabel('Amplitude / dB')
+    plt.xlim([0, 1000])
+    plt.show()
+
+def plot_mfcc(y, sr, name):
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=12)
+    librosa.display.specshow(mfccs, sr=sr, x_axis='time', y_axis='log')
+    plt.title(name)
+    plt.show()
+
+def plot_chromagram(y, sr, name):
+    chromagram = librosa.feature.chroma_stft(y=y, sr=sr)
+    librosa.display.specshow(chromagram, sr=sr, x_axis='time', y_axis='log')
+    plt.title(name)
+    plt.show()
+
+def plot_mel_spectrogram(y, sr, name):
+    mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=12)
+    librosa.display.specshow(mel_spectrogram, sr=sr, x_axis='time', y_axis='log')
+    plt.title(name)
+    plt.show()
