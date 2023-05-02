3e1ec357c0e5f8237333126d62ffb716819038a6
kanielv <kanielvicencio@gmail.com>
2023-05-02 19:36:23 -0400

    converted the videos to .wav

--- src/utils/file_format.py
+++ src/utils/file_format.py
@@ -2,7 +2,13 @@
 This file holds functions relating to file formatting
 '''
 
-import librosa
+import os
+import sys
+from moviepy.editor import VideoFileClip
 
-def convert_mp4_to_wav(f_mp4):
+def convert_mp4_to_wav(video_file, output_path):
+
+
+    clip = VideoFileClip(video_file)
+    clip.audio.write_audiofile(output_path)
     
