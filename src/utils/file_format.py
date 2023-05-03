4c77ce1f6a64f68e3757e7c7e9604e77b6760dc4
kanielv <kanielvicencio@gmail.com>
2023-05-02 20:09:09 -0400

    started audio data anaylsis

--- src/utils/file_format.py
+++ src/utils/file_format.py
@@ -8,7 +8,6 @@ from moviepy.editor import VideoFileClip
 
 def convert_mp4_to_wav(video_file, output_path):
 
-
     clip = VideoFileClip(video_file)
     clip.audio.write_audiofile(output_path)
     
